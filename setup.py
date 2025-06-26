4aeb867340abbcbccd4601bc05406446be35b0c2
linh672 <dangnhatlinh0202@gmail.com>
2025-06-26 10:01:45 +0200

    update requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Search Engine Project",
+    version = "0.0.0",
+    author = "Nhat Linh Dang",
+    packages = find_packages(),
+    install_requires = []
+)
